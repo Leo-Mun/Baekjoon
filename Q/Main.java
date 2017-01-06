78b892f17986adc93f510338ec4c9795e287c438
Leo-Mun <mon04@naver.com>
2017-01-06 19:52:03 +0900

    Add files via upload

---
+++ Q/Main.java
@@ -0,0 +1,37 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int Calculate(int n) {
+		int threeKg = 3;
+		int fiveKg = 5;
+		
+		for (int i = 0; threeKg * i <= n; ++i) {
+			for (int j = 0; fiveKg * j <= (n - (threeKg * i)); ++j) {
+
+				if (n - ((threeKg * i) + (fiveKg * j)) == 0) {
+					return i + j;
+				}
+				
+				
+			
+			
+			}
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		System.out.println(Calculate(n));
+		
+}
+}
